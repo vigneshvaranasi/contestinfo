da0370601117f5c67e8a4c63c2a6bd47b0984dd4
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-07-25 00:39:25 +0530

    naturalised:BrowserAccess

--- APIs/Codechef.js
+++ APIs/Codechef.js
@@ -39,6 +39,7 @@ const fetchCodeChefData = async (username) => {
     try {
         browser = await launchBrowser();
         const page = await browser.newPage();
+        await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
         await page.goto(`https://www.codechef.com/users/${username}`, { waitUntil: 'networkidle2' });
         console.log(`Navigated to https://www.codechef.com/users/${username}`);
 
