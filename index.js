4ca5d656f8f8df02259a5b0528ea8d0c2fac0e36
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-07-25 00:15:21 +0530

    imports:codechefAPI

--- index.js
+++ index.js
@@ -14,8 +14,12 @@ app.use('/codeforces', codeforcesRouter);
 const { router: leetcodeRouter, fetchLeetCodeData } = require('./APIs/Leetcode');
 app.use('/leetcode', leetcodeRouter);
 
+// Import Codechef API
+const { router: codechefRouter, fetchCodeChefData } = require('./APIs/Codechef');
+app.use('/codechef', codechefRouter);
+
 // Import students data
-const students = require('./test.json');
+const students = require('./Students.json');
 
 // Endpoint to fetch all students' data from multiple platforms
 app.get('/data', async (req, res) => {
@@ -24,7 +28,7 @@ app.get('/data', async (req, res) => {
         const promises = students.map(async student => {
 
             //  fetch if the username is available
-            if (student.codeforces === "" || student.leetcode === "") {
+            if (student.codeforces === "" || student.leetcode === "" || student.codechef === "") {
                 return {
                     roll: student.roll,
                     error: 'Username not available',
@@ -32,13 +36,14 @@ app.get('/data', async (req, res) => {
             }
             const codeforcesData = await fetchCodeforcesData(student.codeforces);
             const leetcodeData = await fetchLeetCodeData(student.leetcode);
+            const codechefData = await fetchCodeChefData(student.codechef);
             return {
                 roll: student.roll,
                 codeforces: codeforcesData,
                 leetcode: leetcodeData,
+                codechef: codechefData,
             };
         });
-
         // Wait for all promises to resolve
         const allStudentsData = await Promise.all(promises);
         res.json(allStudentsData);
@@ -46,7 +51,6 @@ app.get('/data', async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 });
-
 
 // Assign Port Number to Server
 const port = 4000;
